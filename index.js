29385ee70edec8375d4759d2f65f81a5fd2ddab9
maiara <maiara.15muller@outlook.com>
2023-09-13 20:36:11 -0300

    Projeto final ok

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const arrayDoadores = [];
+const tiposSanguineosValidos = [
+  "A+",
+  "B+",
+  "AB+",
+  "O+",
+  "A-",
+  "B-",
+  "AB-",
+  "O-",
+];
 
 const menu = () => {
   let opcao = Number(
-    prompt(`
-      ===== SISTEMA DE CADASTRO DE DOADORES DE SANGUE =====
+    prompt(`===== SISTEMA DE CADASTRO DE DOADORES DE SANGUE =====
       1. Cadastrar doador
       2. Listar doadores
       3. Buscar doador por tipo sanguíneo
@@ -26,7 +35,7 @@ const menu = () => {
       buscarPorData();
       break;
     case 5:
-      // Sair
+      sair();
       break;
     default:
       alert("Opção inválida. Tente novamente.");
@@ -35,13 +44,32 @@ const menu = () => {
 };
 
 const cadastrarDoador = () => {
-  let nome = prompt("Digite seu nome completo:");
+  let nome = prompt("Digite seu nome completo:").toUpperCase();
   let idade = Number(prompt("Digite sua idade:"));
-  let peso = Number(prompt("Digite seu peso:"));
-  let tipoSanguineo = prompt("Digite o seu tipo sanguineo");
-  let ultimaDoacao = prompt("Digite a data da sua última doação:");
+  while (isNaN(idade)) {
+    alert("A idade deve ser um numero");
+    idade = Number(prompt("Digite sua idade:"));
+  }
 
-  //const { nome1, idade1, peso1, tipoSanguineo1, ultimaDoacao1 } = doador;
+  let peso = Number(prompt("Digite seu peso:"));
+  while (isNaN(peso)) {
+    alert("O peso deve ser um numero");
+    peso = Number(prompt("Digite seu peso:"));
+  }
+
+  let tipoSanguineo = prompt("Digite o seu tipo sanguineo").toUpperCase();
+  while (!tiposSanguineosValidos.includes(tipoSanguineo)) {
+    alert("Tipo sanguíneo inválido, tente novamente");
+    tipoSanguineo = prompt("Digite o seu tipo sanguineo").toUpperCase();
+  }
+
+  let ultimaDoacao;
+  do {
+    ultimaDoacao = prompt("Digite a data da sua última doação (dd/mm/aaaa):");
+    if (!/^\d{2}\/\d{2}\/\d{4}$/.test(ultimaDoacao)) {
+      alert("Formato de data inválido. Use o formato dd/mm/aaaa.");
+    }
+  } while (!/^\d{2}\/\d{2}\/\d{4}$/.test(ultimaDoacao));
 
   const doador = {
     nome1: nome,
@@ -82,33 +110,74 @@ const listarDoadores = () => {
 };
 
 const buscarTipoSangue = () => {
-  let tipoSangue = prompt("Digite qual o tipo sanguineo você deseja:");
+  let tipoSangue = prompt("Digite o tipo sanguíneo desejado:").toUpperCase();
 
-  if (tipoSangue === "-A" || "+A") {
-    console.log(`
-    
-    
-    
-    
-    `);
+  let doadorPorTipo = arrayDoadores.filter(
+    (doador) => doador.tipoSanguineo1 === tipoSangue
+  );
+
+  let lista = "---------------------\n";
+  lista += "RESULTADO DA BUSCA::\n";
+  lista += "---------------------\n";
+  lista += "NOME             | IDADE | PESO | TIPO SANGUÍNEO | ÚLTIMA DOAÇÃO\n";
+  lista +=
+    "-----------------------------------------------------------------\n";
+  if (doadorPorTipo.length === 0) {
+    console.log("Nenhum doador encontrado com o tipo sanguíneo especificado.");
   } else {
-    if (tipoSangue === "-B" || "+B") {
-      console.log(`
-        
-    `);
-    } else {
-      if (tipoSangue === "-AB" || "+AB") {
-        console.log(`
-    
-    `);
-      }
+    for (let i = 0; i < arrayDoadores.length; i++) {
+      const doador = arrayDoadores[i];
+      lista += `${doador.nome1.padEnd(17)} | ${doador.idade1
+        .toString()
+        .padStart(5)} | ${doador.peso1
+        .toString()
+        .padStart(4)} | ${doador.tipoSanguineo1.padEnd(15)} | ${
+        doador.ultimaDoacao1
+      }\n`;
     }
   }
+  lista +=
+    "-----------------------------------------------------------------\n";
 
+  console.log(lista);
   menu();
 };
 
 const buscarPorData = () => {
+  let dataDesejada = prompt("Digite a data desejada (dd/mm/aaaa): ");
+  let dataUltimaDoacao = arrayDoadores.filter(
+    (doador) => doador.ultimaDoacao1 === dataDesejada
+  );
+
+  let lista = "---------------------\n";
+  lista += "RESULTADO DA BUSCA::\n";
+  lista += "---------------------\n";
+  lista += "NOME             | IDADE | PESO | TIPO SANGUÍNEO | ÚLTIMA DOAÇÃO\n";
+  lista +=
+    "-----------------------------------------------------------------\n";
+  if (dataUltimaDoacao.length === 0) {
+    console.log("Nenhum doador encontrado na data especificada.");
+  } else {
+    for (let i = 0; i < arrayDoadores.length; i++) {
+      const doador = arrayDoadores[i];
+      lista += `${doador.nome1.padEnd(17)} | ${doador.idade1
+        .toString()
+        .padStart(5)} | ${doador.peso1
+        .toString()
+        .padStart(4)} | ${doador.tipoSanguineo1.padEnd(15)} | ${
+        doador.ultimaDoacao1
+      }\n`;
+    }
+  }
+  lista +=
+    "-----------------------------------------------------------------\n";
+
+  console.log(lista);
   menu();
 };
+const sair = () => {
+  alert("Programa encerrado, volte sempre!");
+  menu();
+};
+
 menu();
